8da807f3bbb8c0a15f5bdc86016235abac7d08d2
Kien Le <129171097+kianlecode@users.noreply.github.com>
2025-04-08 00:15:13 +0700

    Update admin.py

--- api/admin.py
+++ api/admin.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends, HTTPException, Request
+from fastapi import APIRouter, HTTPException
 from pydantic import BaseModel
 from models.db import get_db_connection
 import bcrypt
@@ -11,13 +11,6 @@ def get_current_vietnam_time():
     tz = pytz.timezone("Asia/Ho_Chi_Minh")
     return datetime.now(tz).strftime("%Y-%m-%d %H:%M:%S")
 
-# Hàm xác thực người dùng từ session
-def get_current_user(request: Request):
-    user = request.session.get("user")
-    if not user:
-        raise HTTPException(status_code=401, detail="Chưa đăng nhập.")
-    return user
-
 # --------- Schema để tạo giảng viên ---------
 class CreateTeacherRequest(BaseModel):
     user_id: str
@@ -26,20 +19,14 @@ class CreateTeacherRequest(BaseModel):
 
 # --------- API tạo tài khoản giảng viên ---------
 @router.post("/create_teacher")
-def create_teacher(request: Request, 
-                   user_data: CreateTeacherRequest, 
-                   current_user: dict = Depends(get_current_user)):
-
-    if current_user["role"] != "admin":
-        raise HTTPException(status_code=403, detail="Bạn không có quyền thực hiện chức năng này.")
-
+def create_teacher(user_data: CreateTeacherRequest):
     conn = get_db_connection()
     cursor = conn.cursor()
     try:
         # Kiểm tra trùng ID
         cursor.execute("SELECT * FROM users WHERE user_id = ?", (user_data.user_id,))
         if cursor.fetchone():
-            raise HTTPException(status_code=400, detail="User ID đã tồn tại.")
+            raise HTTPException(status_code=400, detail="❌ Mã giảng viên đã tồn tại.")
 
         hashed_pw = bcrypt.hashpw(user_data.password.encode("utf-8"), bcrypt.gensalt()).decode("utf-8")
         now = get_current_vietnam_time()
